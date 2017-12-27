30111b1add3b6bb7db18080f50f1529ebd5ead4c
Aj Wietecha II <ajwietechaii@yahoo.com>
2017-12-26 22:55:12 -1000

    started

--- src/components/homeview/Homeview.js
+++ src/components/homeview/Homeview.js
@@ -6,6 +6,7 @@ class Homeview extends Component {
     return (
       <div className="homecontainer">
         <h1>Home View</h1>
+        <p>I will add some contnent here tomorrow!</p>
       </div>
     );
   }
