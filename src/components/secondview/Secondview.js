c97afe480422a1bd85a0c54702af77d26136cc5c
Aj Wietecha II <ajwietechaii@yahoo.com>
2017-12-27 17:37:37 -1000

    added to mapview

--- src/components/secondview/Secondview.js
+++ src/components/secondview/Secondview.js
@@ -1,14 +1,77 @@
 import React, { Component } from 'react';
 import './Secondview.css';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import Autocomplete from 'react-google-autocomplete';
+
+
 
 class Secondview extends Component {
-  render() {
-    return (
-      <div className="secondcontainer">
-        <h1>Second View</h1>
-      </div>
-    );
-  }
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+    }
 
-export default Secondview;
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onMapClicked = this.onMapClicked.bind(this);
+  }
+
+  onMarkerClick(props, marker, e) {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+
+  onMapClicked(props) {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  }
+  render() {
+      return (
+        <Map google={this.props.google}
+          onClick={this.onMapClicked}
+          className="mapview"
+          initialCenter={{
+            lat: 21.315603,
+            lng: -157.858093
+          }}
+          zoom={12}>
+    <div className="secondcontainer">
+      <h2 className="input-title">Search By Location</h2>
+      <Autocomplete
+        className="search"
+        onPlaceSelected={(place) => {
+          console.log(place);
+        }}
+        types={['(regions)']}
+      />
+    </div>
+
+
+
+          <Marker onClick={this.onMarkerClick}
+                  name={'Honolulu, Hawaii'} />
+                  <InfoWindow
+                 marker={this.state.activeMarker}
+                 visible={this.state.showingInfoWindow}>
+                   <div>
+                     <h4>{this.state.selectedPlace.name}</h4>
+                   </div>
+               </InfoWindow>
+        </Map>
+      );
+    }
+  }
+
+
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyDha-lg4swxWu7TOZfRgiLivhNcjWb4ZT4")
+})(Secondview)
