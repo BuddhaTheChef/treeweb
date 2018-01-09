93bc06eaa262b1705009cb95ec1387d6ac3910f2
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-01-08 14:00:07 -1000

    routed button divs and added more features to home page

--- src/components/homeview/Homeview.js
+++ src/components/homeview/Homeview.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './Homeview.css';
 import pic from './pic.jpg';
 import pic1 from './tree-map.jpg';
@@ -27,7 +28,6 @@ class Homeview extends Component {
 };
     return (
       <div className="homecontainer">
-        {/* <img className="tree-pic" src={pic} alt={"logo"}/> */}
         <div {...props} style={{...defaults, ...style, ...important}}>
           <div className="header-content">
             <img className="banner" src={banner} />
@@ -51,25 +51,31 @@ class Homeview extends Component {
         </div>
         <div className="image-contain">
           <div className="pic-content">
+            <Link to="/secondview"  style={{ textDecoration: 'none' }}>
             <img className="tree-pic1" src={pic1} alt={"logo"}/>
             <h3 className="sub-pic-contnent-header">Find Trees on the Map!</h3>
             <p className="sub-pic-contnent">I will add some contnent here tomorrow! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
               incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
             </p>
+            </Link>
           </div>
           <div className="pic-content">
+            <Link to="/thirdview"  style={{ textDecoration: 'none' }}>
         <img className="tree-pic2" src={pic2} alt={"logo"}/>
         <h3 className="sub-pic-contnent-header">Create a Project!</h3>
         <p className="sub-pic-contnent">I will add some contnent here tomorrow! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
           incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
         </p>
+        </Link>
         </div>
         <div className="pic-content">
+          <Link to="/fourthview"  style={{ textDecoration: 'none' }}>
         <img className="tree-pic3" src={pic3} alt={"logo"}/>
         <h3 className="sub-pic-contnent-header">View 4</h3>
         <p className="sub-pic-contnent">I will add some contnent here tomorrow! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
         </p>
+      </Link>
         </div>
         </div>
       </div>
