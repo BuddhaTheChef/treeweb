f27c043611860e1ac4bdf7b31083531dd56c38b3
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-01-10 21:11:24 -1000

    layput for fourth view

--- src/components/fourthview/Fourthview.js
+++ src/components/fourthview/Fourthview.js
@@ -5,8 +5,14 @@ class Fourthview extends Component {
   render() {
     return (
       <div className="fourthcontainer">
+        <div className="fourthview-content-top">
         <h1 className="title">Fourth View</h1>
       </div>
+
+      <div className="fourthview-content-bot">
+        <p>Still dont know what to do for the content yet</p>
+      </div>
+    </div>
     );
   }
 }
