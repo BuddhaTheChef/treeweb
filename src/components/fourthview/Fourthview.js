02da1c3c27f585839da65b2c7d54ccef0a097fde
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-01-12 13:36:05 -1000

    changing layout of fourth page

--- src/components/fourthview/Fourthview.js
+++ src/components/fourthview/Fourthview.js
@@ -3,7 +3,10 @@ import './Fourthview.css';
 
 class Fourthview extends Component {
   render() {
-    return (
+    return (<div className="main-container">
+      <div className="left-container">
+        hello
+      </div>
       <div className="fourthcontainer">
         <div className="fourthview-content-top">
         <h1 className="title">Fourth View</h1>
@@ -12,6 +15,7 @@ class Fourthview extends Component {
       <div className="fourthview-content-bot">
         <p>Still dont know what to do for the content yet</p>
       </div>
+    </div>
     </div>
     );
   }
