3eb33f13f4754b733891b1af7505b1e292067650
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-01-14 15:28:54 -1000

    styled fourth view

--- src/components/fourthview/Fourthview.js
+++ src/components/fourthview/Fourthview.js
@@ -22,21 +22,12 @@ class Fourthview extends Component {
           <input className="fourthview-input1" placeholder="Search by Location"/>
           <input className="fourthview-input2" placeholder="Search by Code #"/>
         </div>
+        <div className="fourthview-content-bottom">
+          <h1>Projects will be displayed here!</h1>
+        </div>
       </div>
 
-      <div className="fourthview-content-bot">
-        <h2>Still dont know what to do for the content yet</h2>
-      </div>
-    </div>
-    <div className="fourth-right-container">
-      <p>hello</p>
-      <p>hello</p>
-      <p>hello</p>
-      <p>hello</p>
-      <p>hello</p>
-      <p>hello</p>
-      <p>hello</p>
-      <p>hello</p>
+
     </div>
     </div>
     );
